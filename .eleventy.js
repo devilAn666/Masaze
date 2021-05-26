9049df3027b05189d49a1e421d6b49971e843d34
Andrea Doubková <andy.doubek@seznam.cz>
2021-05-26 23:53:41 +0200

    propojení šablon

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy("images");
+    eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("scss");
+
+    return {
+    
+        templateFormats: ["njk","html", "md", "liquid"],
+        
+        markdownTemplateEngine: "njk",
+        htmlTemplateEngine: "njk",
+        dataTemplateEngine: "njk",
+    }
+
+};
